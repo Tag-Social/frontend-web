69c0af09203e4c3ffd72db29e42c38a8a0efe2df
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-30 23:19:29 -0500

    Created pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React  from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {Container} from '@material-ui/core'
 
+import { Navbar } from './components';
 import Home from './pages/Home'
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-      </Switch>
+      <Navbar/>
+      <Container style={{marginTop: '5rem'}}>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+          <Route exact path='/register'>
+            <Register />
+          </Route>
+        </Switch>
+      </Container>
     </Router>
   );
 }
