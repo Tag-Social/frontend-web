b6f63477002d04c35a6718b3b76cc44027240ab3
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2021-01-01 13:13:56 -0500

    Aded Skeleton to Profile loading state

--- src/routes/Profiles/Profiles.tsx
+++ src/routes/Profiles/Profiles.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { RootStateOrAny, useSelector } from 'react-redux';
-import { useFirestoreConnect } from 'react-redux-firebase';
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase';
 import {
     Container,
     CssBaseline,
-    Grid
+    Grid,
 } from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
 
-import { useStyles } from './styles'
+import { useStyles } from './styles';
 import { ProfileHeader, ProfileInfo, Bug } from '../../components';
 
 type Params = {
-    profileId: string
-}
+    profileId: string;
+};
 
 // TODO: Finish
 const UserProfile = () => {
-    let { profileId } = useParams<Params>()
-    useFirestoreConnect(() => [
-        { collection: 'users', doc: profileId }
-    ])
+    let { profileId } = useParams<Params>();
+    useFirestoreConnect(() => [{ collection: 'users', doc: profileId }]);
     const profile = useSelector(
-        ({ firestore: { data } }: RootStateOrAny) => data.users && data.users[profileId]
-    )
-    const classes = useStyles()
+        ({ firestore: { data } }: RootStateOrAny) =>
+            data.users && data.users[profileId]
+    );
+    const classes = useStyles();
     let headerItems = profile && {
         displayName: profile.displayName,
         photoURL: profile.photoURL,
@@ -33,23 +33,37 @@ const UserProfile = () => {
         organization: profile.organization,
         mentor: profile.mentor,
         bio: profile.bio,
+    };
+
+    if (profile) {
+        document.title = 'Tag | ' + profile.displayName;
     }
-
-
-    if (profile) { document.title = 'Tag | ' + profile.displayName; }
 
     return (
         <Container maxWidth='md' className={classes.container}>
             <CssBaseline />
-            <Grid container spacing={2} >
-                {profile ? (<>
+            <Grid container spacing={2}>
+                {!isLoaded(profile) ? (
                     <Grid item xs={12} sm={12} className={classes.gridItem}>
-                        <ProfileHeader headerItems={headerItems} />
+                        <Skeleton variant="rect" height={150} />
+                        <Skeleton variant="circle" width={150} height={150} />
+                        <Skeleton variant="rect" height={300} />
                     </Grid>
-                    <Grid item xs={12} sm={12} className={classes.gridItem}>
-                        <ProfileInfo profile={profile} />
-                    </Grid>
-                </>) : <Bug message='Sorry this profile was not found...' errorType={404} />}
+                ) : isLoaded(profile) && profile ? (
+                    <>
+                        <Grid item xs={12} sm={12} className={classes.gridItem}>
+                            <ProfileHeader headerItems={headerItems} />
+                        </Grid>
+                        <Grid item xs={12} sm={12} className={classes.gridItem}>
+                            <ProfileInfo profile={profile} />
+                        </Grid>
+                        </>
+                    ) : (
+                            <Bug
+                                message='Sorry this profile was not found...'
+                                errorType={404}
+                            />
+                        )}
             </Grid>
         </Container>
     );
