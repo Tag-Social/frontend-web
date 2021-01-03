ecc33f2f2195c6ff51da1cf59beba318c035c889
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2021-01-03 14:16:24 -0500

    updated

--- src/components/profileInfo/editEdu.tsx
+++ src/components/profileInfo/editEdu.tsx
@@ -6,16 +6,19 @@ import {
     Grid,
     TextField,
     Button,
+    Typography,
     Dialog,
     DialogActions,
     DialogContent,
-    DialogContentText,
     DialogTitle,
+    Divider,
     useMediaQuery,
     useTheme,
 } from '@material-ui/core';
+import { Add, Delete } from '@material-ui/icons';
 
-import { userProfile, UserProfile } from '../../firebase/utils/userProfile';
+import { useStyles } from './styles'
+import { Education } from '../../firebase/utils/userProfile';
 
 type Props = {
     open: boolean;
@@ -26,42 +29,126 @@ const EditEdu = ({ open, setOpen }: Props) => {
     const firebase = useFirebase();
     const { profile } = useSelector((state: RootStateOrAny) => state.firebase);
     const theme = useTheme();
+    const classes = useStyles()
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-    const [profileData, setProfileData] = useState<UserProfile>(userProfile);
+    const [eduData, setEduData] = useState<any[]>([]);
 
     useEffect(() => {
-        setProfileData(profile);
+        setEduData(profile.education);
     }, [profile]);
 
     const onChange = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+        index: number
     ) => {
-        setProfileData({
-            ...profile,
-            education: {
-                ...profile.education,
-                [e.target.name]: e.target.value,
+        setEduData([...eduData.map((edu: any, i: number) => {
+            if (index === i) {
+                return {
+                    ...edu,
+                    [e.target.name]: e.target.value
+                } 
+            } else {
+                return edu
             }
-        });
+
+        })])
     };
 
 
     const save = () => {
-        firebase.updateProfile(profileData);
+        firebase.updateProfile({ ...profile, education: eduData });
         setOpen(false);
     };
 
     const handleClose = () => {
         setOpen(false);
+        setEduData(profile.education)
     };
 
-    type Education = {
-        fieldOfStudy: string;
-        school: string;
-        degree: string;
-        endYear: number;
-        startYear: number;
-    };
+    const addEdu = () => {
+        setEduData([
+            ...eduData,
+            {
+                fieldOfStudy: '',
+                school: '',
+                degree: '',
+                endYear: undefined,
+                startYear: undefined,
+            }
+        ])
+    }
+
+    const deleteEdu = (index: number) => {
+        setEduData([...eduData.filter((edu: any, i: number) =>
+            index !== i
+        )])
+    }
+
+    const eduView = eduData && eduData.map((edu: Education, index: number) => (
+        <React.Fragment key={`edu-${index}`}>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    type='text'
+                    name='school'
+                    label='School'
+                    onChange={(e) => onChange(e, index)}
+                    value={edu.school}
+                    fullWidth
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    type='text'
+                    name='fieldOfStudy'
+                    label='Field of Study'
+                    onChange={(e) => onChange(e, index)}
+                    value={edu.fieldOfStudy}
+                    fullWidth
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    type='text'
+                    name='degree'
+                    label='Degree'
+                    onChange={(e) => onChange(e, index)}
+                    value={edu.degree}
+                    fullWidth
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    type='text'
+                    name='startYear'
+                    label='Start Year'
+                    value={edu.startYear}
+                    onChange={(e) => onChange(e, index)}
+                    fullWidth
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    type='text'
+                    name='endYear'
+                    label='End Year'
+                    value={edu.endYear}
+                    onChange={(e) => onChange(e, index)}
+                    fullWidth
+                />
+            </Grid><Grid item xs={12}>
+                <Button onClick={() => deleteEdu(index)} >
+                    <Delete color='error' fontSize='small' />
+                    <Typography variant='caption' color='error'>
+                        Remove
+                    </Typography>
+                </Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Divider />
+            </Grid>
+        </React.Fragment>
+    ))
+
 
     return (
         <Dialog
@@ -72,63 +159,18 @@ const EditEdu = ({ open, setOpen }: Props) => {
             aria-labelledby='responsive-dialog-title'
         >
             <DialogTitle id='responsive-dialog-title'>
-                {"Edit Info"}
+                {"Edit Education"}
             </DialogTitle>
             <DialogContent>
-                <DialogContentText>
-                    {profileData.education && profileData.education[0] && <Grid container spacing={3}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='school'
-                                label='School'
-                                onChange={onChange}
-                                value={profileData.education[0].school}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='fieldOfStudy'
-                                label='Field of Study'
-                                onChange={onChange}
-                                value={profileData.education[0].fieldOfStudy}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='degree'
-                                label='Degree'
-                                onChange={onChange}
-                                value={profileData.education[0].degree}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='startYear'
-                                label='Start Year'
-                                value={profileData.education[0]?.startYear}
-                                onChange={onChange}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='endYear'
-                                label='End Year'
-                                value={profileData.education[0].endYear}
-                                onChange={onChange}
-                                fullWidth
-                            />
-                        </Grid>
-                    </Grid>}
-                </DialogContentText>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Button onClick={addEdu} color='primary'>
+                            <Add fontSize='small' />
+                            Add Education
+                    </Button>
+                    </Grid>
+                    {eduView}
+                </Grid>
             </DialogContent>
             <DialogActions>
                 <Button autoFocus onClick={handleClose} color='primary'>
