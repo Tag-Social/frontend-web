4933d279927ed137ba99f84b537a9d3a90a4ebab
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2021-01-04 22:15:27 -0500

    bug fix

--- src/components/profileHeader/editHeader.tsx
+++ src/components/profileHeader/editHeader.tsx
@@ -13,7 +13,6 @@ import {
     Dialog,
     DialogActions,
     DialogContent,
-    DialogContentText,
     DialogTitle,
     useMediaQuery,
     useTheme,
@@ -53,7 +52,7 @@ const EditHeader = ({ open, setOpen }: Props) => {
             location: {
                 ...profile.location,
                 [e.target.name]: e.target.value,
-            }
+            },
         });
     };
 
@@ -75,146 +74,144 @@ const EditHeader = ({ open, setOpen }: Props) => {
             aria-labelledby='responsive-dialog-title'
         >
             <DialogTitle id='responsive-dialog-title'>
-                {"Edit Info"}
+                {'Edit Info'}
             </DialogTitle>
             <DialogContent>
-                <DialogContentText>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                label='Name'
-                                fullWidth
-                                value={profileData.displayName}
-                                onChange={onChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='email'
-                                name='email'
-                                label='Email'
-                                onChange={onChange}
-                                value={profileData.email}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='pronouns'
-                                label='Pronouns'
-                                onChange={onChange}
-                                value={profileData.pronouns}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={12}>
-                            <TextField
-                                type='text'
-                                name='bio'
-                                label='Bio'
-                                onChange={onChange}
-                                value={profileData.bio}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='organization'
-                                label='Organization'
-                                onChange={onChange}
-                                value={profileData.organization}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='occupation'
-                                label='Occupation'
-                                onChange={onChange}
-                                value={profileData.occupation}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={12}>
-                            <Typography variant='body1'>Gender</Typography>
-                            <RadioGroup
-                                row
-                                name='gender'
-                                value={profileData.gender}
-                                onChange={onChange}
-                            >
-                                <FormControlLabel
-                                    value='female'
-                                    control={<Radio color='primary' />}
-                                    label='Female'
-                                />
-                                <FormControlLabel
-                                    value='male'
-                                    control={<Radio color='primary' />}
-                                    label='Male'
-                                />
-                                <FormControlLabel
-                                    value='non-binary'
-                                    control={<Radio color='primary' />}
-                                    label='Non-Binary'
-                                />
-                                <FormControlLabel
-                                    value='other'
-                                    control={<Radio color='primary' />}
-                                    label='Other'
-                                />
-                                <FormControlLabel
-                                    value='N/A'
-                                    control={<Radio color='primary' />}
-                                    label='N/A'
-                                />
-                            </RadioGroup>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='state'
-                                label='State'
-                                onChange={changeLocation}
-                                value={profileData.location?.state}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='province'
-                                label='Province'
-                                onChange={changeLocation}
-                                value={profileData.location?.province}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='text'
-                                name='country'
-                                label='Country'
-                                onChange={changeLocation}
-                                value={profileData.location?.country}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                type='number'
-                                name='postalCode'
-                                label='Postal Code'
-                                onChange={changeLocation}
-                                value={profileData.location?.postalCode}
-                                fullWidth
-                            />
-                        </Grid>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label='Name'
+                            fullWidth
+                            value={profileData.displayName}
+                            onChange={onChange}
+                        />
                     </Grid>
-                </DialogContentText>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type='email'
+                            name='email'
+                            label='Email'
+                            onChange={onChange}
+                            value={profileData.email}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type='text'
+                            name='pronouns'
+                            label='Pronouns'
+                            onChange={onChange}
+                            value={profileData.pronouns}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <TextField
+                            type='text'
+                            name='bio'
+                            label='Bio'
+                            onChange={onChange}
+                            value={profileData.bio}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type='text'
+                            name='organization'
+                            label='Organization'
+                            onChange={onChange}
+                            value={profileData.organization}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type='text'
+                            name='occupation'
+                            label='Occupation'
+                            onChange={onChange}
+                            value={profileData.occupation}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <Typography variant='body1'>Gender</Typography>
+                        <RadioGroup
+                            row
+                            name='gender'
+                            value={profileData.gender}
+                            onChange={onChange}
+                        >
+                            <FormControlLabel
+                                value='female'
+                                control={<Radio color='primary' />}
+                                label='Female'
+                            />
+                            <FormControlLabel
+                                value='male'
+                                control={<Radio color='primary' />}
+                                label='Male'
+                            />
+                            <FormControlLabel
+                                value='non-binary'
+                                control={<Radio color='primary' />}
+                                label='Non-Binary'
+                            />
+                            <FormControlLabel
+                                value='other'
+                                control={<Radio color='primary' />}
+                                label='Other'
+                            />
+                            <FormControlLabel
+                                value='N/A'
+                                control={<Radio color='primary' />}
+                                label='N/A'
+                            />
+                        </RadioGroup>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type='text'
+                            name='state'
+                            label='State'
+                            onChange={changeLocation}
+                            value={profileData.location?.state}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type='text'
+                            name='province'
+                            label='Province'
+                            onChange={changeLocation}
+                            value={profileData.location?.province}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type='text'
+                            name='country'
+                            label='Country'
+                            onChange={changeLocation}
+                            value={profileData.location?.country}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type='number'
+                            name='postalCode'
+                            label='Postal Code'
+                            onChange={changeLocation}
+                            value={profileData.location?.postalCode}
+                            fullWidth
+                        />
+                    </Grid>
+                </Grid>
             </DialogContent>
             <DialogActions>
                 <Button autoFocus onClick={handleClose} color='primary'>
