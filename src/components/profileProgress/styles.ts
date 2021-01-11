fee169c206ee872951d6475cd48940ffd0c0ee73
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2021-01-10 19:08:12 -0500

    updated styles

--- src/components/profileProgress/styles.ts
+++ src/components/profileProgress/styles.ts
@@ -2,6 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
     card: {
+        maxWidth: '500px',
+        margin: '0 auto',
         boxShadow: `0px 2px 10px -1px rgba(102, 54, 149,0.2), 
                     0px 1px 1px 0px rgba(102, 54, 149,0.14), 
                     0px 1px 3px 0px rgba(102, 54, 149,0.12)`,
