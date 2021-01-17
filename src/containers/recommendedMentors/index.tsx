577927859f7950e1368367ae3c952ce44452c054
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2021-01-17 16:29:20 -0500

    refactored firestore request

--- src/containers/recommendedMentors/index.tsx
+++ src/containers/recommendedMentors/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { RootStateOrAny, useSelector } from 'react-redux';
-import { useFirestoreConnect } from 'react-redux-firebase';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useFirestore } from 'react-redux-firebase';
 import {
     Card,
     CardActionArea,
@@ -12,97 +11,117 @@ import {
     Button,
     Avatar,
 } from '@material-ui/core';
-import { PersonAdd } from '@material-ui/icons'
+import { PersonAdd } from '@material-ui/icons';
 
 import { useStyles } from './styles';
-import { UserProfile } from '../../firebase/utils/userProfile';
-import { PROFILES } from '../../routes/routePaths'
+import { PROFILES } from '../../routes/routePaths';
 
-
-interface Mentor extends UserProfile {
-    id: string
-}
 // TODO : Add actions to connection buttons and add interests
 const RecommendedMentors = ({ profile, auth }: any) => {
-    const interests = (profile.interests && profile.interests.length > 0) ? profile.interests : ['none']
-    useFirestoreConnect(() => [{
-        collection: 'users',
-        where: [['__name__', '!=', auth.uid]]
-    }]);
-    const mentorsData = useSelector(
-        ({ firestore: { data } }: RootStateOrAny) =>
-            data.users
-    );
+    const interests =
+        profile.interests && profile.interests.length > 0
+            ? profile.interests
+            : ['none'];
+    const firestore = useFirestore();
+    const [mentors, setMentors] = useState<any[]>([]);
 
-    const mentors = (): Mentor[] => {
-        if (mentorsData) {
-            return Object.keys(mentorsData).map(id => ({ ...mentorsData[id], id }))
-        }
-        return []
-    }
+    useEffect(() => {
+        firestore
+            .collection('users')
+            .where('__name__', '!=', auth.uid)
+            //.where('interests', 'in', interests)
+            .get()
+            .then((snapshot) => {
+                const data: React.SetStateAction<any[]> = [];
+                snapshot.forEach((doc) =>
+                    data.push({ ...doc.data(), id: doc.id })
+                );
+                setMentors(data);
+            });
+    }, [firestore, auth.uid]);
 
     const classes = useStyles();
     return (
         <>
-            <Typography variant='h6' component='h1' gutterBottom color='primary'>
+            <Typography
+                variant='h6'
+                component='h1'
+                gutterBottom
+                color='primary'
+            >
                 Recommened Mentors
             </Typography>
             <div className={classes.gridList}>
                 <div></div>
-                {mentors().map((mentor) => (
-                    <Card className={classes.userCard} key={`mentors-${mentor.id}`}>
-                        <CardActionArea
-                            component={Link}
-                            to={`${PROFILES}/${mentor.id}`}
-                            className={classes.actionArea}
+                {mentors.length > 0 ? (
+                    mentors.map((mentor) => (
+                        <Card
+                            className={classes.userCard}
+                            key={`mentors-${mentor.id}`}
                         >
-                            <CardMedia
-                                component='img'
-                                alt='Banner'
-                                height='75'
-                                image='https://firebasestorage.googleapis.com/v0/b/tag-app-81b10.appspot.com/o/images%2Fdefault-banner.jpg?alt=media&token=425fde9f-6ae8-447b-8684-bf458e9a8255'
-                                title='Banner'
-                            />
-                            <Avatar
-                                className={classes.avatarContainer}
-                                alt={mentor.displayName?.toString()}
-                                src={mentor.photoURL}
-                            />
-                            <CardContent>
-                                <Typography
-                                    variant='h6'
-                                    component='h2'
+                            <CardActionArea
+                                component={Link}
+                                to={`${PROFILES}/${mentor.id}`}
+                                className={classes.actionArea}
+                            >
+                                <CardMedia
+                                    component='img'
+                                    alt='Banner'
+                                    height='75'
+                                    image='https://firebasestorage.googleapis.com/v0/b/tag-app-81b10.appspot.com/o/images%2Fdefault-banner.jpg?alt=media&token=425fde9f-6ae8-447b-8684-bf458e9a8255'
+                                    title='Banner'
+                                />
+                                <Avatar
+                                    className={classes.avatarContainer}
+                                    alt={mentor.displayName?.toString()}
+                                    src={mentor.photoURL}
+                                />
+                                <CardContent>
+                                    <Typography variant='h6' component='h2'>
+                                        {mentor.displayName}
+                                    </Typography>
+                                    <Typography
+                                        variant='body2'
+                                        color='textSecondary'
+                                        component='p'
+                                    >
+                                        {mentor.occupation}
+                                    </Typography>
+                                    <Typography
+                                        variant='body2'
+                                        color='textSecondary'
+                                        component='p'
+                                    >
+                                        {`${mentor.location.state ||
+                                            mentor.location.province
+                                            }${(mentor.location.state ||
+                                                mentor.location.province) &&
+                                            ','
+                                            } ${mentor.location.country}`}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <PersonAdd fontSize='small' color='primary' />
+                                <Button
+                                    variant='contained'
+                                    color='primary'
+                                    disableElevation
                                 >
-                                    {mentor.displayName}
-                                </Typography>
-                                <Typography
-                                    variant='body2'
-                                    color='textSecondary'
-                                    component='p'
-                                >
-                                    {mentor.occupation}
-                                </Typography>
-                                <Typography
-                                    variant='body2'
-                                    color='textSecondary'
-                                    component='p'
-                                >
-                                    {`${(mentor.location.state || mentor.location.province)}${(mentor.location.state || mentor.location.province) && ','} ${mentor.location.country}`}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <PersonAdd fontSize='small' color='primary' />
-                            <Button variant='contained' color='primary' disableElevation>
-                                Connect
-                            </Button>
-                            <Button variant='outlined'>Follow</Button>
-                        </CardActions>
-                    </Card>
-                ))}
+                                    Connect
+                                </Button>
+                                <Button variant='outlined'>Follow</Button>
+                            </CardActions>
+                        </Card>
+                    ))
+                ) : (
+                        <Typography variant='body1'>
+                            Sorry we have not found any mentors for you yet...
+                        </Typography>
+                    )}
             </div>
         </>
     );
-}
+};
 
-export default RecommendedMentors
+export default RecommendedMentors;
