d062aba99e68402898981abe2b770ac86ee5eaec
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2021-01-24 15:48:52 -0500

    completed hooks

--- src/hooks/useRelationships.ts
+++ src/hooks/useRelationships.ts
@@ -1,11 +1,6 @@
 import { useFirestore } from 'react-redux-firebase';
 import { RootStateOrAny, useSelector } from 'react-redux';
-
-type RelationshipObject = {
-    relId: string;
-    uid: string;
-};
-
+import { Relationship } from '../firebase/utils/relationships';
 const useRelationships = (currentUser: string) => {
     const firestore = useFirestore();
     const { followers, following, mentors, mentees, pending } = useSelector(
@@ -14,36 +9,40 @@ const useRelationships = (currentUser: string) => {
 
     const follow = (userId: string) => {
         // Follow  = type: 0
-        const exist = following.payload.some(
-            (rel: RelationshipObject) => rel.uid === userId
+        const exists = following.some(
+            (rel: Relationship) => rel.uid2 === userId
         );
-        if (!exist) {
+
+        if (!exists) {
             firestore.collection('relationships').add({
                 uid1: currentUser,
                 uid2: userId,
                 type: 0,
+                status: 1,
             });
         }
     };
 
-    const unfollow = (relationshipId: string) => {
-        firestore
-            .collection('relationship')
-            .doc(relationshipId)
-            .update({ status: 3 });
+    const unfollow = (relationshipId: string | undefined) => {
+        const exists = following.some(
+            (rel: Relationship) => rel.relId === relationshipId
+        );
+
+        if (exists)
+            firestore
+                .collection('relationships')
+                .doc(relationshipId)
+                .update({ status: 3 });
     };
 
     const requestMentorship = (mentorId: string) => {
         // Mentorship = type: 1
-        const exist =
+        const exists =
             pending.mentors.some(
-                (rel: RelationshipObject) => rel.uid === mentorId
-            ) ||
-            mentors.payload.some(
-                (rel: RelationshipObject) => rel.uid === mentorId
-            );
+                (rel: Relationship) => rel.uid1 === mentorId
+            ) || mentors.some((rel: Relationship) => rel.uid1 === mentorId);
 
-        if (!exist) {
+        if (!exists) {
             firestore.collection('relationships').add({
                 uid1: mentorId,
                 uid2: currentUser,
@@ -53,21 +52,33 @@ const useRelationships = (currentUser: string) => {
         }
     };
 
-    const acceptMentorship = (uid: string, relationshipId: string) => {
+    const acceptMentorship = (relationshipId: string) => {
         // Accept mentorship = status: 1
-        firestore
-            .collection('relationship')
-            .where('__name__', '==', relationshipId)
-            .where('uid2', '==', uid)
-            .get()
-            .then((snapshot) => {
-                if (!snapshot.empty) {
-                    firestore
-                        .collection('relationship')
-                        .doc(relationshipId)
-                        .update({ status: 1 });
-                }
-            });
+        const exists = pending.mentees.some(
+            (rel: Relationship) => rel.relId === relationshipId
+        );
+
+        if (exists)
+            firestore
+                .collection('relationships')
+                .doc(relationshipId)
+                .update({ status: 1 });
+    };
+
+    const terminateMentorship = (relationshipId: string) => {
+        // Mentorship = type: 1
+        const exists = [
+            ...mentors,
+            ...mentees,
+            ...pending.mentors,
+            ...pending.mentees,
+        ].some((rel: Relationship) => rel.relId === relationshipId);
+
+        if (exists)
+            firestore
+                .collection('relationships')
+                .doc(relationshipId)
+                .update({ status: 3 });
     };
 
     return {
@@ -75,6 +86,7 @@ const useRelationships = (currentUser: string) => {
         unfollow,
         requestMentorship,
         acceptMentorship,
+        terminateMentorship,
     };
 };
 
